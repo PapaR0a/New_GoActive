98184ce8aadbe2535f9c0b2c12fd912ad8794335
Toan Le <lntoan123@gmail.com>
2022-10-06 13:12:09 +0700

    Add link.xml file for skipping StripEngine code on IOS

--- Assets/4.ThirdParties/TaggleMobileUI/Editor/PostBuild.cs
+++ Assets/4.ThirdParties/TaggleMobileUI/Editor/PostBuild.cs
@@ -1,57 +1,57 @@
-﻿#if UNITY_IOS
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
-using UnityEditor.Callbacks;
-using BeliefEngine.UnityEditor.Xcode;
-using System.IO;
+﻿//#if UNITY_IOS
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
+//using UnityEditor.Callbacks;
+//using System.IO;
+//using UnityEditor.iOS.Xcode;
 
-public class HAPostProcessBuild
-{
+//public class HAPostProcessBuild
+//{
     
-	[PostProcessBuild(999)]
-    public static void OnPostProcessBuild( BuildTarget buildTarget, string path)
-    {
-        if(buildTarget == BuildTarget.iOS)
-        {
-            string projectPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+//	[PostProcessBuild(999)]
+//    public static void OnPostProcessBuild( BuildTarget buildTarget, string path)
+//    {
+//        if(buildTarget == BuildTarget.iOS)
+//        {
+//            string projectPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
 
-            PBXProject pbxProject = new PBXProject();
-            pbxProject.ReadFromFile(projectPath);
+//            PBXProject pbxProject = new PBXProject();
+//            pbxProject.ReadFromFile(projectPath);
 
-            string targetName = PBXProject.GetUnityTargetName();
-            string target = pbxProject.TargetGuidByName(targetName);            
-            pbxProject.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
-            pbxProject.AddFrameworkToProject(target, "CoreBluetooth.framework", false);
+//            string targetName = PBXProject.GetUnityTargetName();
+//            string target = pbxProject.TargetGuidByName(targetName);            
+//            pbxProject.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
+//            pbxProject.AddFrameworkToProject(target, "CoreBluetooth.framework", false);
 
-            // reference: http://educoelho.com/unity/2015/06/15/automating-unity-builds-with-cocoapods/
-            // add cocoa support for firebase
-            pbxProject.AddBuildProperty (target, "HEADER_SEARCH_PATHS", "$(inherited)");
-            pbxProject.AddBuildProperty (target, "FRAMEWORK_SEARCH_PATHS", "$(inherited)");
-            pbxProject.AddBuildProperty (target, "OTHER_CFLAGS", "$(inherited)");
-            pbxProject.AddBuildProperty (target, "OTHER_LDFLAGS", "$(inherited)");
+//            // reference: http://educoelho.com/unity/2015/06/15/automating-unity-builds-with-cocoapods/
+//            // add cocoa support for firebase
+//            pbxProject.AddBuildProperty (target, "HEADER_SEARCH_PATHS", "$(inherited)");
+//            pbxProject.AddBuildProperty (target, "FRAMEWORK_SEARCH_PATHS", "$(inherited)");
+//            pbxProject.AddBuildProperty (target, "OTHER_CFLAGS", "$(inherited)");
+//            pbxProject.AddBuildProperty (target, "OTHER_LDFLAGS", "$(inherited)");
 
-            // Copy and replace pod file for firebase
-            string podFilePath = "Assets/Firebase/Podfile";
-            string destPath = Path.Combine (path, "Pods", Path.GetFileName (podFilePath));
-            // if (File.Exists(destPath))
-            FileUtil.ReplaceFile(podFilePath, destPath);
-            // else
-            //     File.Copy(podFilePath, destPath);
+//            // Copy and replace pod file for firebase
+//            string podFilePath = "Assets/Firebase/Podfile";
+//            string destPath = Path.Combine (path, "Pods", Path.GetFileName (podFilePath));
+//            // if (File.Exists(destPath))
+//            FileUtil.ReplaceFile(podFilePath, destPath);
+//            // else
+//            //     File.Copy(podFilePath, destPath);
 
-            pbxProject.WriteToFile (projectPath);
+//            pbxProject.WriteToFile (projectPath);
 
-            // write entitlements
-            string entitlementPath = pbxProject.GetBuildPropertyForConfig(target, "CODE_SIGN_ENTITLEMENTS");
-            var capabilityManager = new ProjectCapabilityManager(projectPath, entitlementPath, targetName);
-            capabilityManager.AddAssociatedDomains(new string[]{
-                "applinks:tagglehealthapp.page.link",
-                "applinks:taggle.page.link"
-            });
+//            // write entitlements
+//            string entitlementPath = pbxProject.GetBuildPropertyForConfig(target, "CODE_SIGN_ENTITLEMENTS");
+//            var capabilityManager = new ProjectCapabilityManager(projectPath, entitlementPath, targetName);
+//            capabilityManager.AddAssociatedDomains(new string[]{
+//                "applinks:tagglehealthapp.page.link",
+//                "applinks:taggle.page.link"
+//            });
 
-            // 
-            capabilityManager.WriteToFile();
-        }
-    }
-}
-#endif
+//            // 
+//            capabilityManager.WriteToFile();
+//        }
+//    }
+//}
+//#endif

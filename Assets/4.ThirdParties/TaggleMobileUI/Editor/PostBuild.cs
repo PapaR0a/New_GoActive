04b9580cc66a9c35797cf4f77639ccdc7ab8a441
Toan Le <lntoan123@gmail.com>
2022-10-10 16:36:41 +0700

    Update PostBuild and project setting for IOS

--- Assets/4.ThirdParties/TaggleMobileUI/Editor/PostBuild.cs
+++ Assets/4.ThirdParties/TaggleMobileUI/Editor/PostBuild.cs
@@ -1,57 +1,38 @@
-﻿//#if UNITY_IOS
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEditor;
-//using UnityEditor.Callbacks;
-//using System.IO;
-//using UnityEditor.iOS.Xcode;
+﻿#if UNITY_IOS
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.IO;
+using UnityEditor.iOS.Xcode;
 
-//public class HAPostProcessBuild
-//{
-    
-//	[PostProcessBuild(999)]
-//    public static void OnPostProcessBuild( BuildTarget buildTarget, string path)
-//    {
-//        if(buildTarget == BuildTarget.iOS)
-//        {
-//            string projectPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+public class HAPostProcessBuild
+{
+    private const string CAMERA_USAGE_DESCRIPTION = "This app require to use camera";
+    private const string LOCATION_USAGE_DESCRIPTION = "This app require to use location";
+    [PostProcessBuild(999)]
+    public static void OnPostProcessBuild(BuildTarget buildTarget, string path)
+    {
+        if (buildTarget == BuildTarget.iOS)
+        {
+            string projectPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+            string plistPath = Path.Combine(path, "Info.plist");
 
-//            PBXProject pbxProject = new PBXProject();
-//            pbxProject.ReadFromFile(projectPath);
+            PBXProject pbxProject = new PBXProject();
+            pbxProject.ReadFromFile(projectPath);
 
-//            string targetName = PBXProject.GetUnityTargetName();
-//            string target = pbxProject.TargetGuidByName(targetName);            
-//            pbxProject.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
-//            pbxProject.AddFrameworkToProject(target, "CoreBluetooth.framework", false);
+            string target = pbxProject.GetUnityMainTargetGuid();
+            // ==== Binary compiled with ARC and Stack smashing flag
+            PlistDocument plist = new PlistDocument();
+            plist.ReadFromString(File.ReadAllText(plistPath));
+            PlistElementDict rootDict = plist.root;
 
-//            // reference: http://educoelho.com/unity/2015/06/15/automating-unity-builds-with-cocoapods/
-//            // add cocoa support for firebase
-//            pbxProject.AddBuildProperty (target, "HEADER_SEARCH_PATHS", "$(inherited)");
-//            pbxProject.AddBuildProperty (target, "FRAMEWORK_SEARCH_PATHS", "$(inherited)");
-//            pbxProject.AddBuildProperty (target, "OTHER_CFLAGS", "$(inherited)");
-//            pbxProject.AddBuildProperty (target, "OTHER_LDFLAGS", "$(inherited)");
-
-//            // Copy and replace pod file for firebase
-//            string podFilePath = "Assets/Firebase/Podfile";
-//            string destPath = Path.Combine (path, "Pods", Path.GetFileName (podFilePath));
-//            // if (File.Exists(destPath))
-//            FileUtil.ReplaceFile(podFilePath, destPath);
-//            // else
-//            //     File.Copy(podFilePath, destPath);
-
-//            pbxProject.WriteToFile (projectPath);
-
-//            // write entitlements
-//            string entitlementPath = pbxProject.GetBuildPropertyForConfig(target, "CODE_SIGN_ENTITLEMENTS");
-//            var capabilityManager = new ProjectCapabilityManager(projectPath, entitlementPath, targetName);
-//            capabilityManager.AddAssociatedDomains(new string[]{
-//                "applinks:tagglehealthapp.page.link",
-//                "applinks:taggle.page.link"
-//            });
-
-//            // 
-//            capabilityManager.WriteToFile();
-//        }
-//    }
-//}
-//#endif
+          
+            //Set NSCameraUsageDescription
+            rootDict.SetString("NSCameraUsageDescription", CAMERA_USAGE_DESCRIPTION);
+            //Set NSLocationWhenInUseUsageDescription
+            rootDict.SetString("NSLocationWhenInUseUsageDescription", LOCATION_USAGE_DESCRIPTION);
+        }
+    }
+}
+#endif
